7ee203bde08d6431c4a1e613d968c49a7daa6d64
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-21 17:00:14 +0530

    ss1

---
+++ data_types/primitive_data_types/ss1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    signed short a = 30000, b = 2000;  // Within short range
+    printf("Signed short addition: %d\n", a + b);
+    printf("Signed short subtraction: %d\n", a - b);
+    printf("Signed short multiplication: %d\n", a * b);
+    printf("Signed short division: %d\n", a / b);
+    return 0;
+}
+
