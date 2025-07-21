b4c0100f4df4a209c6f86791e473a4a905fec123
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-21 17:26:29 +0530

    us1

---
+++ data_types/primitive_data_types/us1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    unsigned short a = 60000, b = 5000;
+    printf("Unsigned short addition: %u + %u = %u\n", a, b, a + b);
+    printf("Unsigned short subtraction: %u - %u = %u\n", a, b, a - b);
+    printf("Unsigned short multiplication: %u * %u = %u\n", a, b, a * b);
+    printf("Unsigned short division: %u / %u = %u\n", a, b, a / b);
+    return 0;
+}
+
