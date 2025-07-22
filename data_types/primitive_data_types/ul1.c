f7e29322d05eec01c0be039701abae4275f63b71
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 11:17:52 +0530

    ul1

---
+++ data_types/primitive_data_types/ul1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    unsigned long a = 3000000000UL, b = 1000000000UL;
+    printf("Unsigned long addition: %lu\n", a + b);
+    printf("Unsigned long subtraction: %lu\n", a - b);
+    printf("Unsigned long multiplication: %lu\n", a * b);
+    printf("Unsigned long division: %lu\n", a / b);
+    return 0;
+}
+
