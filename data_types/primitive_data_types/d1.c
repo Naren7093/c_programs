11f968311870c7cc755421e73af5e8165b7fd51b
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 12:31:52 +0530

    d1

---
+++ data_types/primitive_data_types/d1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    double a = 10.55, b = 2.0;
+    printf("Double addition: %lf\n", a + b);
+    printf("Double subtraction: %lf\n", a - b);
+    printf("Double multiplication: %lf\n", a * b);
+    printf("Double division: %lf\n", a / b);
+    return 0;
+}
+
