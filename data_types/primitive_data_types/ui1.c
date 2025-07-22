6e56d681dba7d907a628453699de172479dde82e
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 10:53:10 +0530

    ui1

---
+++ data_types/primitive_data_types/ui1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    unsigned int x = 4000000000U, y = 2000000000U;
+    printf("Unsigned int addition: %u\n", x + y);
+    printf("Unsigned int subtraction: %u\n", x - y);
+    printf("Unsigned int multiplication: %u\n", x * y);
+    printf("Unsigned int division: %u\n", x / y);
+    return 0;
+}
+
