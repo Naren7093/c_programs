44c9a28dbc8e15e49a0edd0022a4f8f4b5f85797
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 11:41:59 +0530

    sc1

---
+++ data_types/primitive_data_types/sc1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    signed char a = 100, b = 50;
+    printf("Signed char addition: %d + %d = %d\n", a, b, a + b);
+    printf("Signed char subtraction: %d - %d = %d\n", a, b, a - b);
+    printf("Signed char multiplication: %d * %d = %d\n", a, b, a * b);
+    printf("Signed char division: %d / %d = %d\n", a, b, a / b);
+    return 0;
+}
+
