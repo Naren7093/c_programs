b7db605807a1ed3596a13836be5c12a0c83b5af4
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 12:08:39 +0530

    f1

---
+++ data_types/primitive_data_types/f1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float a = 5.5f, b = 2.2f;
+    printf("Float addition: %.2f\n", a + b);
+    printf("Float subtraction: %.2f\n", a - b);
+    printf("Float multiplication: %.2f\n", a * b);
+    printf("Float division: %.2f\n", a / b);
+    return 0;
+}
+
