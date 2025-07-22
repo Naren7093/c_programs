17a257901faae3ea126aa7d1ddc198d0082d1970
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 10:45:12 +0530

    si1

---
+++ data_types/primitive_data_types/si1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 2000000000, y = 1000000000;
+    printf("Signed int addition: %d\n", x + y);
+    printf("Signed int subtraction: %d\n", x - y);
+    printf("Signed int multiplication: %d\n", x * y);
+    printf("Signed int division: %d\n", x / y);
+    return 0;
+}
+
