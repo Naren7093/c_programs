e00aaa2e774ff1de7586f3ee5ab5cdebe0c51afb
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 11:49:16 +0530

    uc1

---
+++ data_types/primitive_data_types/uc1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    unsigned char a = 250, b = 50;
+    printf("Unsigned char addition: %u + %u = %u\n", a, b, a + b);
+    printf("Unsigned char subtraction: %u - %u = %u\n", a, b, a - b);
+    printf("Unsigned char multiplication: %u * %u = %u\n", a, b, a * b);
+    printf("Unsigned char division: %u / %u = %u\n", a, b, a / b);
+    return 0;
+}
+
