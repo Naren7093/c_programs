ef7cb48de31e8e21364f0196e041b0b159d9daf6
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-22 11:08:08 +0530

    sl1

---
+++ data_types/primitive_data_types/sl1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    long a = 1000000000L, b = 500000000L;
+    printf("Signed long addition: %ld\n", a + b);
+    printf("Signed long subtraction: %ld\n", a - b);
+    printf("Signed long multiplication: %ld\n", a * b);
+    printf("Signed long division: %ld\n", a / b);
+    return 0;
+}
+
