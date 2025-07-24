35eeb92334032123a0b23c554a69af56aefb941c
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-24 11:23:26 +0530

    for

---
+++ control_statements/for.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int count;
+
+    
+    for(count=1; count<=10; count++)
+    {
+        
+        printf("%d ", count);
+    }
+
+    return 0;
+}
