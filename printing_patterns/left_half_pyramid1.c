db798aa5cacd5acc87bb5ac1ed6f254b1859dfa4
Narendra <pinnintinarendra7093494583@gamil.com>
2025-07-28 12:55:44 +0530

    left_half_pyramid1

---
+++ printing_patterns/left_half_pyramid1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int k,l,m;
+	for(k=1;k<=6;k++)
+	{
+		for(l=1;l<=6-k;l++)
+		{
+			printf(" ");
+		}
+		for(m=1;m<=k;m++)
+		{
+			printf("%d",m);
+		}
+		printf("\n");
+	}
+	return 0;
+}
