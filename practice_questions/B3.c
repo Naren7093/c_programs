7286567d9e46ec306e946675f39c74b15ce74dc0
Narendra <pinnintinarendra7093494583@gamil.com>
2025-08-05 18:34:27 +0530

    B3

---
+++ practice_questions/B3.c
@@ -0,0 +1,16 @@
+// set i'th bit of a number
+#include<stdio.h>
+
+int main()
+{
+	int b,i;
+	printf("Enter a number:");
+	scanf("%d",&b);
+
+	printf("Enter a position:");
+	scanf("%d",&i);
+
+	b=b|(1<<i);
+	printf("after adding set bit :%d\n",b);
+	return 0;
+}
