ee5792f2ac6a6afa3c3128dcbba413c8aca55029
Narendra <pinnintinarendra7093494583@gamil.com>
2025-08-05 18:37:13 +0530

    B5

---
+++ practice_questions/B5.c
@@ -0,0 +1,17 @@
+// Toggle i'th bit of a number
+#include<stdio.h>
+
+int main()
+{
+        int b,i;
+        printf("Enter a number:");
+        scanf("%d",&b);
+
+        printf("Enter a position:");
+        scanf("%d",&i);
+
+        b=b^(1<<i);
+        printf("after toggle ith bit :%d\n",b);
+        return 0;
+}
+
