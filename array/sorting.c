622b67ecffd369849406611a962d012c5cddb05c
Narendra <pinnintinarendra7093494583@gamil.com>
2025-08-14 15:10:25 +0530

    sorting

---
+++ array/sorting.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {5, 3, 8, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i, j, temp;
+
+    
+    for (i =0; i < n-1; i++)
+    {
+        for (j =i+1; j < n; j++)
+        {
+            if (arr[i] > arr[j]) 
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array in ascending order:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
