064a8e0a21fe8c8044581780a0ba8296a830c6cc
Naren7093 <pinnintinarendra7093494583@gmail.com>
2025-12-04 12:24:30 +0530

    vowels_consonants

---
+++ c/vowels_consonants.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char str[20];
+	int vowel=0;
+	int consonant=0;
+	printf("enter a string:");
+	fgets(str,sizeof(str),stdin);
+	str[strcspn(str,"\n")]='\0';
+
+	for(int i=0;str[i]!='\0';i++){
+		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+		printf(" vowel:%c ->ASCII:%d \n ",str[i],str[i]);
+		vowel++;
+	}
+		else if((str[i]>='a'&& str[i]<='z')||(str[i]>='A'&&str[i]<='Z')){
+       		printf("consonant:%c ->ASCII:%d \n",str[i],str[i]);
+		consonant++;
+		}
+	}
+
+	printf("\nVowels in string :%d\n",vowel);
+	printf("consonants in string :%d\n",consonant);
+	
+	return 0;
+	}
+
+
+	
+
+
